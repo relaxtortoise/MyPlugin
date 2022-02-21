8beee116a5ad568ace2cb81241682b7d95fe4f93
GitHub Action <action@github.com>
2022-02-21 09:40:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/relaxtortoise/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/relaxtortoise/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.relaxtortoise.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.relaxtortoise.myplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/relaxtortoise/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/relaxtortoise/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.relaxtortoise.myplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.relaxtortoise.myplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
